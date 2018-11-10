5a8cb09621df67120b65c05c9048fc924a46ef1a
jonbwalker <jonathan.b.walker0@gmail.com>
2018-11-10 13:04:45 -0800

    add sass loader and node sass

---
+++ components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="anim">
+    Hi There
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar'
+  }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="container">
     <div>
+      <nav-bar></nav-bar>
       <app-logo/>
       <h1 class="site-title font-weight-thin">
         jonbwalker.com
@@ -24,42 +25,41 @@
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
+import NavBar from "../components/NavBar";
 
 export default {
   components: {
+    NavBar,
     AppLogo
   }
 }
 </script>
 
-<style scoped>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="sass" scoped>
+.container
+  min-height: 100vh
+  display: flex
+  justify-content: center
+  align-items: center
+  text-align: center
 
-.site-title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+.site-title
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif /* 1 */
+  display: block
+  font-weight: 300
+  font-size: 100px
+  color: #35495e
+  letter-spacing: 1px
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+.subtitle
+  font-weight: 300
+  font-size: 42px
+  color: #526488
+  word-spacing: 5px
+  padding-bottom: 15px
 
-.links {
-  padding-top: 15px;
-}
+.links
+  padding-top: 15px
+
 </style>
 
