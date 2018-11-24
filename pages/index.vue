4dd366f41981ee1b95a7209b2c381bc4f8a8f23e
jonbwalker <jonathan.b.walker0@gmail.com>
2018-11-24 13:55:57 -0600

    add coming soon blur overlay

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,53 @@
 <template>
-  <b-container>
-    <b-row>
-      <nav-bar/>
-    </b-row>
+  <div>
+    <div class="coming-soon">
+      <div class="coming-soon-text">
+        coming soon
+      </div>
+    </div>
+    <b-container>
+      <b-row>
+        <nav-bar/>
+      </b-row>
 
-    <b-row>
-      <b-col cols="12">
-        <app-logo/>
-      </b-col>
-    </b-row>
+      <b-row>
+        <b-col cols="12">
+          <app-logo/>
+        </b-col>
+      </b-row>
 
-    <b-row>
-      <b-col cols="12">
-        <h1 class="site-title font-weight-thin">
-          jonbwalker.com
-        </h1>
-      </b-col>
-    </b-row>
+      <b-row>
+        <b-col cols="12">
+          <h1 class="site-title font-weight-thin">
+            jonbwalker.com
+          </h1>
+        </b-col>
+      </b-row>
 
-    <b-row>
-      <b-col cols="12">
-        <h2 class="subtitle">
-          personal site built with vue.js
-        </h2>
-      </b-col>
-    </b-row>
+      <b-row>
+        <b-col cols="12">
+          <h2 class="subtitle">
+            personal site built with vue.js
+          </h2>
+        </b-col>
+      </b-row>
 
-    <b-row class="links">
-      <b-col cols="6">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-      </b-col>
-      <b-col cols="6">
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </b-col>
-    </b-row>
-  </b-container>
+      <b-row class="links">
+        <b-col cols="6">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            class="button--green">Documentation</a>
+        </b-col>
+        <b-col cols="6">
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="button--grey">GitHub</a>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -55,7 +62,46 @@
 </script>
 
 <style lang="sass" scoped>
+  .coming-soon
+    margin: 0 auto
+    display: flex
+    justify-content: center
+    align-content: center
+    position: fixed /* Sit on top of the page content */
+    width: 100% /* Full width (cover the whole page) */
+    height: 100% /* Full height (cover the whole page) */
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+    z-index: 2
+
+  .coming-soon-text
+    position: absolute
+    top: 70%
+    left: 50%
+    color: #2972a5
+    font-size: calc(45px + (100 - 25) * ((100vw - 300px) / (1600 - 300)));
+    font-weight: 200
+    transform: translate(-50%,-50%)
+    -ms-transform: translate(-50%,-50%)
+    &::before
+      content: '{'
+      color: #BE8635
+      font-size: 7.3vw
+      padding: 5px
+    &::after
+      content: '}'
+      color: #BE8635
+      font-size: 7.3vw
+      padding: 5px
+
+  html
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif
+    font-weight: 200
+
   .container
+    filter: blur(7px)
     align-items: center
     animation: 3s ease 0s normal forwards 1 fadein
     justify-content: space-around
@@ -63,7 +109,6 @@
 
   .site-title
     color: #2972a5
-    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif
     font-weight: 300
     font-size: 7vw
     letter-spacing: 1px
