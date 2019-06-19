d59d9cfc2de2cebf3e750f96857a9ae9c9eec140
jonbwalker <jonathan.b.walker0@gmail.com>
2019-06-19 15:19:15 -0700

    update key

--- plugins/slForm.js
+++ plugins/slForm.js
@@ -1,10 +1,10 @@
 if (process.browser) {
   window._Sendlane = window._Sendlane || [];
   _Sendlane.push({
-    form_key: 'eyJkb21haW4iOiJ1c2VyIiwiZm9ybV9pZCI6Ijk5NmExZDk5LTdkZTItNGNiNi04ZWM5LWJhOTEyNjQ1NWE4MCJ9'
+    form_key: 'eyJkb21haW4iOiJ1c2VyIiwiZm9ybV9pZCI6IjUyMDY2ZDZhLTU4NjUtNDU5Yi1hNTgyLTllZTY1NjAxYjJiYyJ9'
   });
   let script = window.document.createElement('script');
-  script.src = 'https://sendlane.test/js/form.js';
+  script.src = '"https://sendlane.test/scripts/pusher.js';
   script.async = true;
   let first = document.getElementsByTagName('script')[0];
   first.parentNode.insertBefore(script, first);
