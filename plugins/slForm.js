bbe2b69173835a101f42894f2c2003e80442a0c6
jonbwalker <jonathan.b.walker0@gmail.com>
2019-09-02 12:00:37 -0700

    fix form key

--- plugins/slForm.js
+++ plugins/slForm.js
@@ -1,7 +1,7 @@
 if (process.browser) {
   window._Sendlane = window._Sendlane || [];
   _Sendlane.push({
-    form_key: 'eyJkb21haW4iOiJqb25hdGhhbndhbGtlciIsImZvcm1faWQiOiI5ZmZhYTJhNy1lNzZiLTQ2NTYtODE4Yy1lZWEyODRmZjMyMTEifQ'
+    form_key: 'eyJkb21haW4iOiJqb25hdGhhbndhbGtlciIsImZvcm1faWQiOiI5ZmZhYTJhNy1lNzZiLTQ2NTYtODE4Yy1lZWEyODRmZjMyMTEifQ=='
   });
   let script = window.document.createElement('script');
   script.src = 'https://sendlane.com/scripts/pusher.js';
