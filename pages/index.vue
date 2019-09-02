be4506e293f7be9e85755d27a085ec57559c7b6f
jonbwalker <jonathan.b.walker0@gmail.com>
2019-09-02 14:55:54 -0700

    fix text responsiveness  * update bootstrap-vue nuxt-edge to latest

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container fluid>
-    <b-row class="section-1">
-      <b-col cols="12">
+    <b-row>
+      <b-col cols="4" class="section-1">
         <h1 class="site-title">
           jonbwalker
         </h1>
@@ -9,7 +9,7 @@
     </b-row>
 
     <b-row>
-      <b-col cols="12">
+      <b-col cols="4" class="section-2">
         <h2 class="subtitle">
           web developer
         </h2>
@@ -26,6 +26,8 @@
 </script>
 
 <style lang="sass" scoped>
+  $enable-responsive-font-sizes: true
+
   html
     font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif
     font-weight: 200
@@ -39,30 +41,33 @@
     text-align: center
     height: 100vh
 
-  .site-title
+  .section-1
     background: rgba(255,255,255,0.5)
     border-radius: 25px
-    color: #343a40
-    font-weight: 300
-    font-size: calc(50px + (100 - 50) * ((100vw - 400px) / (1600 - 400)))
-    letter-spacing: 1px
     margin-top: 60px
     margin-right: 32%
     margin-left: 6%
     padding-bottom: 7px
-    width: 34%
 
-  .subtitle
-    color: #85c9f3
+  .site-title
+    color: #343a40
+    font-weight: 300
+    font-size: 6vw
+    letter-spacing: 1px
+
+  .section-2
     background: rgba(0,0,0,0.4)
     border-radius: 25px
-    font-weight: 300
-    font-size: 4vw
     margin-right: 32%
     margin-left: 12%
     padding-bottom: 8px
+
+  .subtitle
+    color: #85c9f3
+    font-weight: 300
+    font-size: 4vw
     word-spacing: 5px
-    width: 34%
+
 
   @keyframes fadein
     0%
@@ -71,5 +76,11 @@
       opacity: 0
     100%
       opacity: 1
+
+  @media screen and (min-width: 1200px)
+    .site-title
+      font-size: 70px
+    .subtitle
+      font-size: 48px
 </style>
 
